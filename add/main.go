1457a927a12b347693820164495ebafbaefaa1ee
Vikram Oruganti <vikramoruganti@gmail.com>
2021-02-14 21:16:05 -0500

    Update main.go

--- add/main.go
+++ add/main.go
@@ -12,6 +12,13 @@ func main() {
 		sum := Add(num1, num2)
 		fmt.Println("The sum in binary equals" + " " + sum)
 	} else {
-		print(valid)
 	}
 }
+
+//Output:
+//Your inputs are valid, computing sum
+//The sum in binary equals 111110111
+
+//If either num1 or num2 contains a digit other than 0 or 1:
+//Output:
+//Your {first/second} input is invalid
